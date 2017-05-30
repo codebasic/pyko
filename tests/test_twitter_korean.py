fb04cdda4350a92329c93931fbaa028bea2adc40
Lee Seongjoo <seongjoo@codebasic.co>
2017-05-30 11:54:45 +0900

    test grouped

--- tests/test_twitter_korean.py
+++ tests/test_twitter_korean.py
@@ -3,27 +3,31 @@ import pytest
 from pyko import OpenKoreanTextProcessor
 
 
-@pytest.fixture()
+@pytest.fixture(scope='module')
 def processor():
     return OpenKoreanTextProcessor()
 
 
-def test_construction():
-    assert OpenKoreanTextProcessor()
-
-
-def test_normalize(processor):
+class TestOpenKoreanTextProcessor:
     text = '한국어를 처리하는 예시입니닼ㅋㅋㅋㅋ'
-    gold = '한국어를 처리하는 예시입니다ㅋㅋㅋ'
 
-    text_normalized = processor.normalize(text)
-    assert text_normalized == gold
+    def test_construction(self):
+        assert OpenKoreanTextProcessor()
 
+    def test_normalize(self, processor):
+        gold = '한국어를 처리하는 예시입니다ㅋㅋㅋ'
+        text_norm = processor.normalize(self.text)
+        assert text_norm == gold
 
-def test_tokenize(processor):
-    text = '한국어를 처리하는 예시입니닼ㅋㅋㅋㅋ'
-    gold = ['한국어', '를', '처리', '하는', '예시', '입니다', 'ㅋㅋㅋ']
+    def test_tokenize(self, processor):
+        gold = ['한국어', '를', '처리', '하는', '예시', '입니다', 'ㅋㅋㅋ']
+        text_norm = processor.normalize(self.text)
+        tokens = processor.tokenize(text_norm)
+        assert tokens == gold
 
-    text_normalized = processor.normalize(text)
-    tokens = processor.tokenize(text_normalized)
-    assert tokens == gold
+    @pytest.mark.skip(reason='Depedency library does not support')
+    def test_stem(self, processor):
+        gold = ['한국어', '를', '처리', '하다', '예시', '이다', 'ㅋㅋㅋ']
+        text_norm = processor.normalize(self.text)
+        stem_tokens = processor.stem(text_norm)
+        assert stem_tokens == gold
