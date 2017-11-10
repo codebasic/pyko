8a2dee161d04319f2559be3c7ff92d9f6d3c06c7
Lee Seongjoo <seongjoo@codebasic.co>
2017-11-10 11:48:23 +0900

    added preprocessor

--- pyko/__init__.py
+++ pyko/__init__.py
@@ -1,9 +1,18 @@
 # coding: utf-8
 import os
+import re
 from pkg_resources import resource_filename
 
 from jpype import getDefaultJVMPath
 import jpype
+
+
+def preprocessor(text):
+    #비문자제거패턴 = re.compile(r'[^ㄱ-ㅣ가-힣a-zA-Z0-9 ]')
+    # ㄷㄷ, ㅋㅋ 과 같은 단어는 선택에서 제외
+    비문자제거패턴 = re.compile(r'[^가-힣a-zA-Z0-9 ]')
+    text = 비문자제거패턴.sub('', text)
+    return text
 
 
 def java_str(func):
@@ -36,3 +45,7 @@ class OpenKoreanTextProcessor:
         tokens = self._processor.tokensToJavaStringList(tokens)
         tokens = [t for t in tokens]
         return tokens
+
+    @java_str
+    def stem(self, text):
+        raise NotImplemented('Dependency API change pending')
--- setup.py
+++ setup.py
@@ -3,29 +3,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyko',
-    version='0.1',
+    version='0.1.1',
     description='Korean Text Processor',
     author='Lee Seongjoo',
     author_email='seongjoo@codebasic.io',
     url='https://github.com/codebasic/pyko',
-    keywords='natural language processing text korean'
+    keywords='natural language processing text korean',
     packages=find_packages(),
     package_data={
         'pyko': ['java/*.jar']
     },
-    classifiers=[
-        'Classifier: Development Status :: 3 - Alpha',
-        'Classifier: License :: OSI Approved :: Apache Software License',
-        'Classifier: Natural Language :: Korean',
-        'Classifier: Operating System :: MacOS',
-        'Classifier: Operating System :: Microsoft :: Windows',
-        'Classifier: Programming Language :: Java',
-        'Classifier: Programming Language :: Python :: 3',
-        'Classifier: Topic :: Scientific/Engineering',
-        'Classifier: Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'Classifier: Topic :: Software Development :: Libraries :: Java Libraries',
-        'Classifier: Topic :: Software Development :: Version Control :: Git',
-        'Classifier: Topic :: Text Processing',
-        'Classifier: Topic :: Text Processing :: Linguistic'
-    ]
+    install_requires=['jpype1']
 )
