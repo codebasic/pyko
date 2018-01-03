4a93f72cc31ed83b638a45c4ae012f5cc0f4f2c8
Lee Seongjoo <seongjoo@codebasic.co>
2018-01-03 11:38:39 +0900

    단어 토큰 읽기 구현

--- pyko/reader.py
+++ pyko/reader.py
@@ -3,7 +3,7 @@ Copyright (C) 2017 Codebasic
 Author: Lee Seongjoo <seongjoo@codebasic.io>
 """
 
-import os.path
+import os
 from nltk.corpus.reader.api import CorpusReader
 from bs4 import BeautifulSoup
 
@@ -37,22 +37,27 @@ class SejongCorpusReader(CorpusReader):
 
         return sents
 
-    def words(self, fileids=None, tagged=True):
-        fileids = self.get_fileids(fileids)
+    def words(self, fileids=None, tagged=False):
+        fileids = fileids if fileids else self._fileids
 
         words = []
-        if not tagged:
-            raise NotImplemented
 
         for fid in fileids:
-            soup = BeautifulSoup(open(fid, encoding=self.encoding), 'lxml')
+            filepath = os.path.join(self._root, fid)
+            soup = BeautifulSoup(open(filepath, encoding=self._encoding), 'lxml')
             body = soup.find('text')
             sent_tags = body.find_all('s')
 
             for tag in sent_tags:
                 for line in tag.text.split('\n')[1:]:
-                    word = line.split('\t')[1]
-                    if word:
-                        words.append(word)
+                    tagged_tokens = line.split('\t')[-1].split('+')
+                    if len(tagged_tokens) < 2:
+                        continue
+                    tagged_tokens = [tuple(token.split('/')) for token in tagged_tokens]
+
+                    if tagged:
+                        words.extend(tagged_tokens)
+                    else:
+                        words.extend(token for token, tag in tagged_tokens)
 
         return words
