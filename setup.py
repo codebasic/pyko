6a2bc5ea4e2195101b8e309393a61e3ab7f9d213
Lee Seongjoo <seongjoo@codebasic.co>
2018-02-04 22:22:16 +0900

    version 2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyko',
-    version='0.1.1',
+    version='0.2.0',
     description='Korean Text Processor',
     author='Lee Seongjoo',
     author_email='seongjoo@codebasic.io',
