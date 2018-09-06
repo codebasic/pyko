5cf04a18f4fd4ecc4d11d52dfe0751e1d571f8f1
Lee Seongjoo <seongjoo@codebasic.io>
2018-09-06 13:37:50 +0900

    WordSet added with tests.

--- pyko/__init__.py
+++ pyko/__init__.py
@@ -4,13 +4,16 @@ import re
 
 from pkg_resources import resource_filename
 
-
-from jpype import getDefaultJVMPath
-import jpype
+import numpy as np
+import pandas as pd
+from pandas import Series
+from keras.preprocessing.sequence import skipgrams
 
 from . import reader
 
 __all__ = [reader]
+__version__ = '0.4.0'
+
 
 def preprocessor(text):
     #비문자제거패턴 = re.compile(r'[^ㄱ-ㅣ가-힣a-zA-Z0-9 ]')
@@ -29,6 +32,9 @@ def java_str(func):
 
 class OpenKoreanTextProcessor:
     def __init__(self):
+        from jpype import getDefaultJVMPath
+        import jpype
+
         if not jpype.isJVMStarted():
             libpath = resource_filename(__name__, 'java')
             jars = os.listdir(libpath)
@@ -53,4 +59,61 @@ class OpenKoreanTextProcessor:
 
     @java_str
     def stem(self, text):
-        raise NotImplemented('Dependency API change pending')
+        raise NotImplementedError('Dependency API change pending')
+
+
+class WordSet:
+    def __init__(self, texts, tokenizer=None, select='^[ㄱ-ㅣ가-힣0-9]+'):
+        self.select = select      
+        # 0번 색인은 UNK(모름, 없음)으로 예약
+        self.word_set = ['UNK']
+        if tokenizer:
+            tokens = tokenizer.tokenize(texts)
+        else:
+            tokens = texts
+        self._build_word_set(tokens, select=select)
+        
+    def __repr__(self):
+        return repr(self.word_set)
+    
+    def __len__(self):
+        return len(self.word_set)
+    
+    def __getitem__(self, i):
+        return self.word_set[i]
+    
+    def _build_word_set(self, tokens, select):
+        vocab = []
+        for token in tokens:
+            if re.match(select, token) and not token in vocab:
+                vocab.append(token)
+        vocab.sort()
+        self.word_set += vocab
+        self.word_set = Series(self.word_set)
+
+    def get_word_index(self, word=None, pattern=None):
+        if word:
+            matched = self.word_set[self.word_set == word]
+            return matched.index[0] if len(matched) else 0
+        
+        elif pattern:
+            matched = self.word_set[self.word_set.str.match(pattern)]
+            return dict(matched) if len(matched) else 0
+
+        else:
+            raise ValueError('word 또는 pattern 둘 중에 하나를 지정해야 합니다.')
+        
+    def text_to_word_id_sequence(self, texts, select=None):
+        pattern = select if select else self.select        
+        for text in texts:
+            id_seq = [self.get_word_index(token) for token in text if re.match(pattern, token)]
+            yield id_seq
+            
+    def generate_skipgrams(self, texts):
+        vocab_size = len(self.word_set)
+        word_index_seqs = self.text_to_word_id_sequence(texts)
+        for wid_seq in word_index_seqs:
+            if len(wid_seq) < 2:
+                continue
+            pairs, labels = skipgrams(wid_seq, vocab_size)            
+            yield pairs, labels
