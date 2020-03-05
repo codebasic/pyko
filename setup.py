3c2977f81159928428b14b750c41fc657249ac5c
Lee Seongjoo <seongjoo@codebasic.io>
2020-03-05 10:13:27 +0900

    버전 0.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyko',
-    version='0.3.0',
+    version='0.4.0',
     description='Korean Text Processor',
     author='Lee Seongjoo',
     author_email='seongjoo@codebasic.io',
