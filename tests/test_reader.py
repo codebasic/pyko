c11616768b42c9e41b50916a23c9919a24a6659d
Lee Seongjoo <seongjoo@codebasic.io>
2020-03-10 01:22:03 +0000

    세종말뭉치 테스트 구조 정리

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -1,25 +1,23 @@
 import os
-import re
 import pytest
 from pyko.reader import SejongCorpusReader
 
 
-class TestSejong:
-    CORPUS_ROOT = os.environ['CORPUS_ROOT']
-    CORPUS_PATH = os.path.join(CORPUS_ROOT, 'sejong')
+CORPUS_ROOT = os.environ['CORPUS_ROOT']
+SEJONG_CORPUS_PATH = os.path.join(CORPUS_ROOT, 'sejong')
+
+@pytest.fixture(scope='module', params=[r'spoken/word_tag/.+\.txt'])
+def 세종말뭉치(request):
+    return SejongCorpusReader(SEJONG_CORPUS_PATH, request.param)
         
-    def test_words(self):        
-        reader = SejongCorpusReader(
-                TestSejong.CORPUS_PATH, 
-                r'spoken/word_tag/.+\.txt', encoding='utf-16')
-        
-        형태분석목록 = reader.words(tagged=True)
-        assert len(형태분석목록)
-        
-        for 어절, 형태분석 in 형태분석목록:
-            assert 어절
-            assert len(형태분석)
-            for 형태소, 품사 in 형태분석:
-                assert 형태소
-                assert 품사
+def test_words(세종말뭉치):
+    형태분석목록 = 세종말뭉치.words(tagged=True)
+    assert len(형태분석목록)
+    
+    for 어절, 형태분석 in 형태분석목록:
+        assert 어절
+        assert len(형태분석)
+        for 형태소, 품사 in 형태분석:
+            assert 형태소
+            assert 품사
         
