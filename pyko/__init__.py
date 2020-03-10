61e6c44dff6beb5679d52a73c0e27aa4f579c2f7
Lee Seongjoo <seongjoo@codebasic.io>
2020-03-10 07:38:48 +0000

    __init__.py 정리.

--- pyko/__init__.py
+++ pyko/__init__.py
@@ -1,9 +1,2 @@
 # coding: utf-8
-import os
-import re
-
-from pkg_resources import resource_filename
-
-from . import reader, tokenizer
-
-__all__ = [reader, tokenizer]
+__all__ = ["reader", "tokenizer"]
