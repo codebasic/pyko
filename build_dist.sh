b831cd10ac661a9476392b38ba2917a55af10096
Lee Seongjoo <seongjoo@codebasic.io>
2020-03-13 01:46:33 +0000

    빌드 시, 패키지 버전과 일치 여부 확인.

--- build_dist.sh
+++ build_dist.sh
@@ -1,1 +1,6 @@
-python setup.py sdist bdist_wheel
+PKG_VERSION=`python -c "import pyko; print(pyko.__version__)"`
+if [ $PKG_VERSION == $VERSION ]; then
+    python setup.py sdist bdist_wheel
+else
+    echo "패키지 버전이 환경 변수 설정과 다릅니다."
+fi
